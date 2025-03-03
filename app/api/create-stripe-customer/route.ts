53a592fdc246f950d3acaa2ccb33f07e283e8671
mm1135 <mm1135@users.noreply.github.com>
2025-03-03 10:50:56 +0900

    feat: stripeに顧客情報追加APIの作成

---
+++ app/api/create-stripe-customer/route.ts
@@ -0,0 +1,36 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { NextRequest, NextResponse } from "next/server";
+import { Stripe } from "stripe"
+import { cookies } from "next/headers";
+
+export async function POST(request: NextRequest){
+    const supabase = createRouteHandlerClient({cookies})
+
+    const query = request.nextUrl.searchParams.get("API_ROUTE_SECRET")
+
+    if(query !== process.env.API_ROUTE_SECRET){
+        return NextResponse.json({
+            message: "APIを叩く権限がありません",
+        }, {status: 401})
+    }
+
+    const data = await request.json()
+    const {id, email} = data
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+
+    const customer = await stripe.customers.create({
+        email,
+    })
+
+    const {error} = await supabase.from('profile').update({
+        stripe_customer: customer.id,
+    }).eq('id', id)
+
+    console.log(id, customer.id)
+
+    console.log(error?.message)
+
+    return NextResponse.json({
+        message: `stripe customer created: ${customer.id}`,
+    })
+}
